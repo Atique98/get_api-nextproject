5d22ebed0f4da1a0d5e8c7560cdd170590bb5b4e
Md-Atique-Github <muhammadatique.bscs@gmail.com>
2024-05-05 15:11:11 +0500

    formatting

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,7 +1,7 @@
 import { user } from "@/util/db";
 import { NextResponse } from "next/server";
 
-export function GET(request){
-    const data = user;
-    return NextResponse.json(user,{status:200})
+export function GET(request) {
+  const data = user;
+  return NextResponse.json(user, { status: 200 });
 }
